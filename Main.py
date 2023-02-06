9b4888fc2a30a5a2a5b41c61bda7d8fc549fe0a9
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-06 17:39:01 +0000

    final version

--- Main.py
+++ Main.py
@@ -3,11 +3,16 @@ from pedalboard.io import AudioFile
 
 def change_volume(directory_path, scale_by):
     audio_files = os.listdir(directory_path)
-    for audio_file in audio_files:
-        with AudioFile(audio_file) as f:
-            audio = f.read(f.frames)
-            # by multiplying each value in the array representing the audio by some value
-            #  you scale its volume by that value
-            scaled_audio = audio * scale_by
-        with AudioFile("out_" + audio_file, "w", f.samplerate) as o:
-            o.write(change_volume("testFolder1", 0.2))
+    for i in range (2):
+        for audio_file in audio_files:
+            with AudioFile(directory_path + "/" + audio_file) as f:
+                audio = f.read(f.frames)
+                mono = audio[i]
+                # by multiplying each value in the array representing the audio by some value
+                #  you scale its volume by that value
+                scaled_audio = mono * scale_by
+                with AudioFile("out_" + audio_file, "w", f.samplerate) as o:
+                    o.write(scaled_audio)
+
+change_volume("testFolder1", 2)
+print("Done!")
