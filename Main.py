1852d12404ac55c5294b2bdad5f56f5d5dbebdcd
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-06 17:05:04 +0000

    change_volume now creates a new file instead of overwriting

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 import os
-from pedalboard.pedalboard.io import AudioFile
+from pedalboard.io import AudioFile
 
 def change_volume(directory_path, scale_by):
-    audio_files = os.listdir("soundFiles")
+    audio_files = os.listdir(directory_path)
     for audio_file in audio_files:
-        with AudioFile("Windows XP Startup.wav") as f:
+        with AudioFile(audio_file) as f:
             audio = f.read(f.frames)
             # by multiplying each value in the array representing the audio by some value
             #  you scale its volume by that value
             scaled_audio = audio * scale_by
+        with AudioFile("out_" + audio_file, "w", f.samplerate) as o:
+            o.write(change_volume("testFolder1", 0.2))
