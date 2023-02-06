53fe5d89899b89e2ef3b511730a15eef9a996ef6
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-06 16:57:13 +0000

    change_volume now works by directory

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
+import os
 from pedalboard.pedalboard.io import AudioFile
 
 def change_volume(directory_path, scale_by):
-    with AudioFile("Windows XP Startup.wav") as f:
-        audio = f.read(f.frames)
-        # by multiplying each value in the array representing the audio by some value
-        #  you scale its volume by that value
-        scaled_audio = audio * scale_by
+    audio_files = os.listdir("soundFiles")
+    for audio_file in audio_files:
+        with AudioFile("Windows XP Startup.wav") as f:
+            audio = f.read(f.frames)
+            # by multiplying each value in the array representing the audio by some value
+            #  you scale its volume by that value
+            scaled_audio = audio * scale_by
